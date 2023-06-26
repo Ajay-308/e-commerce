c8b7d78dbc94e39227234ad39e831295e4194113
AJAY SINGH <mr.ajaykumar308@gmail.com>
2023-06-27 01:09:50 +0530

    filter,header and styling things

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
 import Header from "./components/Header";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header />
-    </div>
+      <div className="App">
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,90 @@
-import React from "react";
+import { FaShoppingCart } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
 import {
-  Container,
-  FormControl,
-  Navbar,
   Badge,
-  Nav,
+  Button,
+  Container,
   Dropdown,
+  FormControl,
+  Nav,
+  Navbar,
 } from "react-bootstrap";
+import { Link, useLocation } from "react-router-dom";
+import { CartState } from "../context/Context";
+import "./styles.css";
 
 const Header = () => {
+  const {
+    state: { cart },
+    dispatch,
+    productDispatch,
+  } = CartState();
+
   return (
-    <Navbar bg="dark" varient="light" style={{ height: 95 }}>
+    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
       <Container>
         <Navbar.Brand>
-          <a href="#home">Shopping cart</a>
+          <Link to="/">Shopping Cart</Link>
         </Navbar.Brand>
-        <Navbar.Text className="search">
-          <FormControl
-            style={{ width: 550 }}
-            placeholder="search product"
-            className="m-auto"
-          />
-          {/* //provide auto spacing */}
-        </Navbar.Text>
+        {useLocation().pathname.split("/")[1] !== "cart" && (
+          <Navbar.Text className="search">
+            <FormControl
+              style={{ width: 500 }}
+              type="search"
+              placeholder="Search a product..."
+              className="m-auto"
+              aria-label="Search"
+              onChange={(e) => {
+                productDispatch({
+                  type: "FILTER_BY_SEARCH",
+                  payload: e.target.value,
+                });
+              }}
+            />
+          </Navbar.Text>
+        )}
         <Nav>
           <Dropdown alignRight>
             <Dropdown.Toggle variant="success">
-              <Badge>0</Badge>
+              <FaShoppingCart color="white" fontSize="25px" />
+              <Badge>{cart.length}</Badge>
             </Dropdown.Toggle>
-            <Dropdown.Menu style={{ minWidth: 300 }}>
-              <span style={{ padding: 15 }}>cart is empty</span>
+
+            <Dropdown.Menu style={{ minWidth: 370 }}>
+              {cart.length > 0 ? (
+                <>
+                  {cart.map((prod) => (
+                    <span className="cartitem" key={prod.id}>
+                      <img
+                        src={prod.image}
+                        className="cartItemImg"
+                        alt={prod.name}
+                      />
+                      <div className="cartItemDetail">
+                        <span>{prod.name}</span>
+                        <span>₹ {prod.price.split(".")[0]}</span>
+                      </div>
+                      <AiFillDelete
+                        fontSize="20px"
+                        style={{ cursor: "pointer" }}
+                        onClick={() =>
+                          dispatch({
+                            type: "REMOVE_FROM_CART",
+                            payload: prod,
+                          })
+                        }
+                      />
+                    </span>
+                  ))}
+                  <Link to="/cart">
+                    <Button style={{ width: "95%", margin: "0 10px" }}>
+                      Go To Cart
+                    </Button>
+                  </Link>
+                </>
+              ) : (
+                <span style={{ padding: 10 }}>Cart is Empty!</span>
+              )}
             </Dropdown.Menu>
           </Dropdown>
         </Nav>
